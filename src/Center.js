8a6f29e02c6dd85ddf8c1a14f6100d13a2163fa3
laliv <ll>
2022-01-15 16:43:54 +0100

    .

--- src/Center.js
+++ src/Center.js
@@ -5,27 +5,27 @@ const Center = () => {
   let green = 100;
   let blue = 100;
   const [color, setColor] = useState([red, green, blue]);
-  const changeColor = () => {
-    setInterval(() => {
-      if (red + 1 < 245) {
-        red += 1;
-      } else {
-        red = 100;
-      }
-      if (green + 1 < 245) {
-        green += 1;
-      } else {
-        green = 100;
-      }
-      if (blue + 1 < 245) {
-        blue += 1;
-      } else {
-        blue = 100;
-      }
-      setColor([red, green, blue]);
-    }, 20);
-  };
   useEffect(() => {
+    const changeColor = () => {
+      setInterval(() => {
+        if (red + 1 < 245) {
+          red += 1;
+        } else {
+          red = 100;
+        }
+        if (green + 1 < 245) {
+          green += 1;
+        } else {
+          green = 100;
+        }
+        if (blue + 1 < 245) {
+          blue += 1;
+        } else {
+          blue = 100;
+        }
+        setColor([red, green, blue]);
+      }, 20);
+    };
     changeColor();
   }, []);
   return (
